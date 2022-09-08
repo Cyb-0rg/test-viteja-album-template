72f81576b50228ca851a0e085ddec5e599429a8a
Ibrahim Sadudin Kedir <ibrahimsdudin@gmail.com>
2022-09-08 11:01:19 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+import './App.css'
+
+function App() {
+ 
+  return (
+    <div className="App">
+
+
+      <div className='container'>
+
+        <div className='grid1'>
+
+
+
+        </div>
+
+        <div className='grid1'>
+
+          
+
+        </div>
+
+        <div className='grid1'>
+
+          
+
+        </div>
+
+
+        <div className='grid1'>
+
+          
+
+        </div>
+
+        <div className='grid1'>
+
+          
+
+      </div>
+
+
+      <div className='grid1'>
+
+          
+
+    </div>
+
+    <div className='grid1'>
+
+          
+
+    </div>
+
+
+
+        </div>
+
+        
+
+
+      
+    
+
+
+
+      
+      
+    </div>
+  )
+}
+
+export default App
